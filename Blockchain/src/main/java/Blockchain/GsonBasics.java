17428ed6f5c0c7410ec29999134dc0d016051691
Potti1234 <lukas.pottner@gmail.com>
2023-10-12 19:21:59 +0200

    Added more Tests

--- Blockchain/src/main/java/Blockchain/GsonBasics.java
+++ Blockchain/src/main/java/Blockchain/GsonBasics.java
@@ -18,11 +18,19 @@ public class GsonBasics {
 	
 	@SuppressWarnings("deprecation")
 	public String getNameFromJson(String json) {
+		if(json == null) {
+			throw new NullPointerException("Input json can not be null");
+		}
+		
 		LOG.info("Start");
 		JsonParser parser = new JsonParser();
 		JsonElement element = parser.parse(json);
 		LOG.info("Json erfolgreich geparsed");
 		JsonObject object = element.getAsJsonObject();
+		if(object.get("name") == null) {
+			throw new IllegalArgumentException("Json does not contain name");
+		}
+		
 		String name = object.get("name").getAsString();
 		LOG.info("Name: {}", name);
 		
--- Blockchain/src/test/java/Tests/JsonTest.java
+++ Blockchain/src/test/java/Tests/JsonTest.java
@@ -1,5 +1,6 @@
 package Tests;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,12 +12,43 @@ public class JsonTest {
 	void test() {
 		//given
 		String input = "{\"name\": \"lukas\"}";
+		GsonBasics gsonBasics = new GsonBasics();
 		
 		//when
-		GsonBasics gsonBasics = new GsonBasics();
 		String name = gsonBasics.getNameFromJson(input);
 		
 		//then
 		assertEquals("lukas", name);
+	}
+	
+	@Test
+	void noNameTest() {
+		//given
+		String input = "{\"username\": \"lukas\"}";
+		GsonBasics gsonBasics = new GsonBasics();
+
+		//then
+		assertThrows(IllegalArgumentException.class, () -> gsonBasics.getNameFromJson(input));
+	}
+	
+	@Test
+	void nullTest() {
+		//given
+		String input = null;
+		GsonBasics gsonBasics = new GsonBasics();
+		
+		//then
+		NullPointerException ex = assertThrows(NullPointerException.class, () -> gsonBasics.getNameFromJson(input));
+		assertEquals(ex.getMessage(), "Input json can not be null");
+	}
+	
+	@Test
+	void wrongInputTest() {
+		//given
+		String input = "Bad";
+		GsonBasics gsonBasics = new GsonBasics();
+
+		//then
+		assertThrows(IllegalStateException.class, () -> gsonBasics.getNameFromJson(input));
 	}
 }
